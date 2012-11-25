4f0b7924c602c2f2ac974726513d5de8d997f627
Andrzej Fiedukowicz <fiedukow@gmail.com>
2012-11-25 20:31:26 +0100

    Created BookDetailWindow Form.

---
+++ CrossWindowsApplication/BookDetailsWindow.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace CrossWindowsApplication
+{
+    partial class BookDetailsWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.TitleBox = new System.Windows.Forms.TextBox();
+            this.AuthorBox = new System.Windows.Forms.TextBox();
+            this.RelCal = new System.Windows.Forms.MonthCalendar();
+            this.TypeBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.MyCancelButton = new System.Windows.Forms.Button();
+            this.OkButton = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10.78611F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 89.2139F));
+            this.tableLayoutPanel1.Controls.Add(this.TitleBox, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.AuthorBox, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.RelCal, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.TypeBox, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label4, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.MyCancelButton, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.OkButton, 1, 4);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 52.72727F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 47.27273F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 178F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 28F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(547, 285);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // TitleBox
+            // 
+            this.TitleBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TitleBox.Location = new System.Drawing.Point(62, 3);
+            this.TitleBox.Name = "TitleBox";
+            this.TitleBox.Size = new System.Drawing.Size(482, 20);
+            this.TitleBox.TabIndex = 0;
+            // 
+            // AuthorBox
+            // 
+            this.AuthorBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.AuthorBox.Location = new System.Drawing.Point(62, 30);
+            this.AuthorBox.Name = "AuthorBox";
+            this.AuthorBox.Size = new System.Drawing.Size(482, 20);
+            this.AuthorBox.TabIndex = 1;
+            // 
+            // RelCal
+            // 
+            this.RelCal.CalendarDimensions = new System.Drawing.Size(3, 1);
+            this.RelCal.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.RelCal.Location = new System.Drawing.Point(68, 61);
+            this.RelCal.Name = "RelCal";
+            this.RelCal.TabIndex = 2;
+            // 
+            // TypeBox
+            // 
+            this.TypeBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TypeBox.FormattingEnabled = true;
+            this.TypeBox.Location = new System.Drawing.Point(62, 233);
+            this.TypeBox.Name = "TypeBox";
+            this.TypeBox.Size = new System.Drawing.Size(482, 21);
+            this.TypeBox.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 27);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Title";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(3, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Author";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(3, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 178);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Release Date";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(3, 230);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 26);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Type";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // MyCancelButton
+            // 
+            this.MyCancelButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MyCancelButton.Location = new System.Drawing.Point(3, 259);
+            this.MyCancelButton.Name = "MyCancelButton";
+            this.MyCancelButton.Size = new System.Drawing.Size(53, 23);
+            this.MyCancelButton.TabIndex = 8;
+            this.MyCancelButton.Text = "Cancel";
+            this.MyCancelButton.UseVisualStyleBackColor = true;
+            // 
+            // OkButton
+            // 
+            this.OkButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.OkButton.Location = new System.Drawing.Point(62, 259);
+            this.OkButton.Name = "OkButton";
+            this.OkButton.Size = new System.Drawing.Size(482, 23);
+            this.OkButton.TabIndex = 9;
+            this.OkButton.Text = "Ok";
+            this.OkButton.UseVisualStyleBackColor = true;
+            // 
+            // BookDetailsWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(547, 285);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "BookDetailsWindow";
+            this.Text = "BookDetailsWindow";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+            this.AcceptButton = OkButton;
+            this.CancelButton = CancelButton;
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox TitleBox;
+        private System.Windows.Forms.TextBox AuthorBox;
+        private System.Windows.Forms.MonthCalendar RelCal;
+        private System.Windows.Forms.ComboBox TypeBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button MyCancelButton;
+        private System.Windows.Forms.Button OkButton;
+    }
+}
---
+++ CrossWindowsApplication/BookDetailsWindow.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CrossWindowsApplication
+{
+    public partial class BookDetailsWindow : Form
+    {
+        public BookDetailsWindow()
+        {
+            InitializeComponent();
+        }
+
+        public BookDetailsWindow(String title, String author, DateTime date, int typeId)
+        {
+            InitializeComponent();
+            TitleBox.Text = title;
+            AuthorBox.Text = author;
+            RelCal.SetDate(date);
+            TypeBox.SelectedIndex = typeId;
+        }
+
+        public BookDetailsWindow(Book book)
+        {
+            InitializeComponent();
+            TitleBox.Text = book.Title;
+            AuthorBox.Text = book.Author;
+            RelCal.SetDate(book.ReleaseDate);
+            TypeBox.SelectedIndex = (int) book.Type;
+        }
+
+        public Book buildBook()
+        {
+            return new Book(TitleBox.Text, AuthorBox.Text, RelCal.SelectionStart, (BookType) TypeBox.SelectedIndex);
+        }
+
+        public void fillBook(Book book)
+        {
+            book.Title = TitleBox.Text;
+            book.Author = AuthorBox.Text;
+            book.ReleaseDate = RelCal.SelectionStart;
+            book.Type = (BookType) TypeBox.SelectedIndex;
+        }
+    }
+}
